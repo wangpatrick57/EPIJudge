c0e4bbe1ef016fb5b9ca8bccd353a40c669453f6
Patrick Wang <wang.patrick57@gmail.com>
2023-09-01 21:38:00 -0400

    preorder recursive

--- epi_judge_cpp/tree_preorder.cc
+++ epi_judge_cpp/tree_preorder.cc
@@ -1,15 +1,32 @@
 #include <vector>
+#include <stack>
+#include <utility>
 
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
-using std::vector;
 
-vector<int> PreorderTraversal(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+using namespace std;
+
+void PreorderTraversalRecursive(const unique_ptr<BinaryTreeNode<int>> &node, vector<int> &traversal)
+{
+  if (node == nullptr) {
+    return;
+  }
+
+  traversal.push_back(node->data);
+  PreorderTraversalRecursive(node->left, traversal);
+  PreorderTraversalRecursive(node->right, traversal);
 }
 
-int main(int argc, char* argv[]) {
+vector<int> PreorderTraversal(const unique_ptr<BinaryTreeNode<int>> &tree)
+{
+  vector<int> traversal;
+  PreorderTraversalRecursive(tree, traversal);
+  return traversal;
+}
+
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"tree"};
   return GenericTestMain(args, "tree_preorder.cc", "tree_preorder.tsv",
