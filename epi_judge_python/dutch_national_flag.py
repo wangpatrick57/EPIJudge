e5bf343b321473bef62361a3279d5cefdb19f7db
Patrick Wang <wang.patrick57@gmail.com>
2024-02-24 12:14:42 -0500

    dutch dutch_onlytwoleft

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -8,7 +8,7 @@ from test_framework.test_utils import enable_executor_hook
 RED, WHITE, BLUE = range(3)
 
 
-def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
+def dutch_allthreeleft(pivot_index: int, A: List[int]) -> None:
     pivot = A[pivot_index]
     eq_start_i = 0
     gt_start_i = 0
@@ -33,6 +33,30 @@ def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
             gt_start_i += 1
         else:
             pass
+
+
+def dutch_onlytwoleft(pivot_index: int, A: List[int]) -> None:
+    n = len(A)
+    pivot = A[pivot_index]
+    eq_start_i = 0
+    gt_start_i = n
+    unknown_start_i = 0
+
+    while unknown_start_i < gt_start_i:
+        if A[unknown_start_i] == pivot:
+            unknown_start_i += 1
+        elif A[unknown_start_i] < pivot:
+            A[eq_start_i], A[unknown_start_i] = A[unknown_start_i], A[eq_start_i]
+            eq_start_i += 1
+            unknown_start_i += 1
+        else:
+            gt_start_i -= 1
+            A[gt_start_i], A[unknown_start_i] = A[unknown_start_i], A[gt_start_i]
+            # don't increment unknown_start_i. we already decremented gt_start_i
+
+
+def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
+    dutch_onlytwoleft(pivot_index, A)
 
 
 @enable_executor_hook
