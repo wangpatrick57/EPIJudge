0f89862f5f3e783f3c0a08791997ddeafcdffe3f
Patrick Wang <wang.patrick57@gmail.com>
2024-02-24 10:32:40 -0500

    parity.py

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,25 @@
 from test_framework import generic_test
 
+parity_cache = dict()
+
+def parity_8bit(x: int) -> int:
+    parity = 0
+    for _ in range(8):
+        parity ^= x & 1
+        x >>= 1
+    return parity
 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    global parity_cache
+    parity = 0
+    for _ in range(8):
+        x_8bit = x & 0xFF
+        if x_8bit not in parity_cache:
+            parity_cache[x_8bit] = parity_8bit(x_8bit)
+        x_8bit_parity = parity_cache[x_8bit]
+        parity ^= x_8bit_parity
+        x >>= 8
+    return parity
 
 
 if __name__ == '__main__':
