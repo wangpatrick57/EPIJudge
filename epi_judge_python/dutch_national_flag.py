3073e6c9e1b16e63c2dba6bca8212935c409b1ea
Patrick Wang <wang.patrick57@gmail.com>
2024-02-24 12:06:28 -0500

    dutch allthreeleft

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -9,8 +9,30 @@ RED, WHITE, BLUE = range(3)
 
 
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    pivot = A[pivot_index]
+    eq_start_i = 0
+    gt_start_i = 0
+
+    for i in range(len(A)):
+        if A[i] < pivot:
+            if eq_start_i == gt_start_i == i:
+                pass
+            elif eq_start_i == gt_start_i:
+                A[eq_start_i], A[i] = A[i], A[eq_start_i]
+            elif gt_start_i == i:
+                A[eq_start_i], A[i] = A[i], A[eq_start_i]
+            else:
+                tmp = A[gt_start_i]
+                A[gt_start_i] = A[eq_start_i]
+                A[eq_start_i] = A[i]
+                A[i] = tmp
+            eq_start_i += 1
+            gt_start_i += 1
+        elif A[i] == pivot:
+            A[i], A[gt_start_i] = A[gt_start_i], A[i]
+            gt_start_i += 1
+        else:
+            pass
 
 
 @enable_executor_hook
